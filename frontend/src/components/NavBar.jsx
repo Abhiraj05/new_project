e8ef68741afd12c73cfdc1ee927620e61c269f87
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2025-10-19 16:43:14 +0530

    home page created

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -29,7 +29,7 @@ const NavBar = () => {
               Sign In
             </button>
             <button className="px-4 py-2 bg-primary text-white rounded-lg bg-blue-700 transition-colors">
-              Get Started
+              Sign Up
             </button>
           </nav>
         </div>
