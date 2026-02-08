9e01d899487d4b2580d7424cb52bda64feccf80d
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2026-02-08 23:43:11 +0530

    wrote apis to generate quiz via llm & integrated llm to bakckend

--- backend/django_app/project/main/views.py
+++ backend/django_app/project/main/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 import requests
 
 
-@api_view(['POST'])nnmmnm
+@api_view(['POST'])
 def generate_quiz(request):
     user_input=request.data.get("text_data")
     data = {
